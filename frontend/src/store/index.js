d310ae473ed610246e4e1b022c8ccc76a3184988
Miguel <mchirinot@gmail.com>
2022-05-24 23:27:38 +0200

    cleaning

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,10 @@
 import { createStore } from 'vuex';
 
-import pools_info from './modules/pools_info'
 import contract from './modules/contract'
 import controller from './modules/controller'
 
 export default createStore({
   modules: {
-    pools_info,
     contract,
     controller
   },
