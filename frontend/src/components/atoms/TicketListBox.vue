31387cb24018c2c34835d3567b51e1e74282411c
MiguelCh <mchirinot@gmail.com>
2022-11-26 10:51:51 -0500

    ticket list css

--- frontend/src/components/atoms/TicketListBox.vue
+++ frontend/src/components/atoms/TicketListBox.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="list">
     <div v-for="(ticket, index) in ticketData" :key="index" class="ticket">
-      <span class="label">{{STRINGS.ticketAmount}}<span class="text">{{ticket.amount}}{{STRINGS.eth}}</span></span>
-      <span class="label">{{STRINGS.ticketEntryDate}}<span class="text">{{ticket.timestamp}}</span></span>
-      <span class="label">{{STRINGS.ticketWalletAddress}}<span class="text">{{ticket.ticketOwner}}</span></span>
+      <div class="ticket-wrapper">
+        <div class="text-container">
+          <h4 class="label">{{STRINGS.ticketAmount}}</h4>
+          <span class="text">{{ticket.amount}}{{STRINGS.eth}}</span>
+        </div>
+        <div class="text-container">
+          <h4 class="label">{{STRINGS.ticketEntryDate}}</h4>
+          <span class="text">{{ticket.timestamp}}</span>  
+        </div>
+        <div class="text-container">
+          <h4 class="label">{{STRINGS.ticketWalletAddress}}</h4>
+          <span class="text">{{ticket.ticketOwner}}</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -28,7 +39,7 @@ export default {
 <style scoped lang="scss">
 .list {
   font-family: "Roboto Mono", monospace;
-  font-size: 10px;
+  font-size: 12px;
   text-align: left;
   margin: 11px 7px;
   padding: 0;
@@ -43,15 +54,26 @@ export default {
   & .ticket {
     margin: 0;
     padding: 4px;
-    border-bottom: 1px solid lightgrey;
-    background-color: #f5f5f5;
-    display: flex;
+    background-color: #fff;
   }
   & .label {
-    font-weight: bold;
+    font-weight: bolder;
+    margin: 0;
+    width: 111px;
   }
   & .text {
     font-weight: normal;
+    margin: 0;
+  }
+}
+.ticket-wrapper {
+  padding: 4px;
+  border-bottom: 1px solid lightgrey;
+  border-left: 1px dashed lightgrey;
+  background-color: #f5f5f5;
+
+  .text-container {
+    display: flex;
   }
 }
 </style>
