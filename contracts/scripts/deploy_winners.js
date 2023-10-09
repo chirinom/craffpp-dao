c301d0e31180379a294338b0d59f7fab3d5cd7bd
MiguelCh <mchirinot@gmail.com>
2023-10-09 16:09:06 -0500

    deploy

--- contracts/scripts/deploy_winners.js
+++ contracts/scripts/deploy_winners.js
@@ -18,4 +18,4 @@ const runMain = async () => {
 runMain();
 
 // To peploy. From /contract
-// npx hardhat run --network goerli scripts/deploy.js
+// npx hardhat run --network goerli scripts/deploy_winners.js
